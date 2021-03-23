60fa8a3fc8accbd1aeaf75b129b92f0ea555c150
shrutisusan26 <shrutisusan055@gmail.com>
2021-03-23 22:26:11 +0530

    Basic create & join implementation

--- thread.c
+++ thread.c
@@ -17,15 +17,16 @@
 char *stackTop;
 queue *q;
 static int flag = 0;
-static struct thread_lock lock;//
+static struct thread_lock lock;
 /* something along these lines needs to be implemented
 1. Allocate stack space for each thread 
 2. Stack +stacksize is done because s
 tack grows downwards
  */
 int thread_create(thread_t *thread, void *(*start_routine)(void *), void *arg){
+    int status;
     thread_s * t=(thread_s *)calloc(1,sizeof(thread_s));
-   // t->stack = (char *)malloc(STACKSIZE);
+    // t->stack = (char *)malloc(getpagesize());
     t->stack= mmap(NULL, STACKSIZE * sizeof(char), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     if (t->stack == NULL){
         return EAGAIN;
@@ -34,14 +35,16 @@ int thread_create(thread_t *thread, void *(*start_routine)(void *), void *arg){
     t->start_routine = start_routine;
     t->arg = arg;
     t->state = RUNNING;
-    t->t_id = clone((int (*)(void *))start_routine, stackTop, CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SYSVSEM | CLONE_SIGHAND | CLONE_THREAD | CLONE_SETTLS | CLONE_PARENT_SETTID | CLONE_CHILD_CLEARTID | 0, t->arg);
-    //printf("%d", t->t_id);
+    t->t_id = clone((int (*)(void *))start_routine, stackTop,SIGCHLD | CLONE_FS | CLONE_FILES |CLONE_SIGHAND | CLONE_VM , t->arg);
+    // printf("%d ",getpid());
+    
     if (t->t_id == -1)
     {
         free(t->stack);
+        free(t);
         return EAGAIN;
     }
-    printf("%s", stackTop);
+    // printf("%s", stackTop);
     if (flag==0){
     	flag=1;
     	q=(queue *)calloc(1,sizeof(queue));
@@ -58,7 +61,8 @@ int thread_join(thread_t thread, void ** retval){
 		if (retthread->state==TERMINATED){
 			return EINVAL;
 		}
-		waitpid(thread,NULL,0);
+		// waitpid(thread,NULL,__WCLONE);
+        wait(&thread);
 		retthread->state=TERMINATED;
 		if (retval){
 			*retval=retthread->ret;
@@ -72,12 +76,16 @@ int thread_join(thread_t thread, void ** retval){
 }
 
 void *func(){
-    fprintf(stderr, "%s\n\n\n\n", "hi");
     sleep(10);
+    printf("%d ",getpid());
+    printf("%s\n\n\n\n", "hi");
+    // sleep(10);
+    
 }
 
 int main()
 {
+    
     thread_t thread;
     thread_create(&thread, func, NULL);
     thread_join(thread,NULL);
